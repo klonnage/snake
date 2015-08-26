fabd7ac78b545af35ae6b725e9085e8951c7be4e
klonnage <vinc.brdionneau@gmail.com>
2015-08-26 22:39:48 +0200

    gere l'affichage

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,1 +1,50 @@
 #include "camera.hpp"
+#include "constantes.hpp"
+#include <vector>
+#include <utility>
+
+void Camera::draw()
+{
+	//get a view on the cells of the map
+	std::vector<CellContent> const& cells = data.map.getCells();
+	
+	//get the map dimension
+	std::pair<int, int> dimension = data.map.getDimension();
+	int i = 0, colon = dimension.second;
+	
+	//clear the window
+	window.clear(sf::Color::White);
+	
+	//draw the map
+	for(std::vector<CellContent>::const_iterator 
+		it = cells.begin();	it != cells.end();it++, i++)
+	{
+		CellContent content = *it;
+		tile.setPosition((float)((i%colon)*50),
+			(float)((i/colon)*50));
+		switch(content)
+		{
+			case WALL:
+				tile.setFillColor(sf::Color(100,50,30));
+				break;
+			case EMPTY:
+				tile.setFillColor(sf::Color::White);
+				break;
+			case SNAKE_BODY:
+				tile.setFillColor(sf::Color::Yellow);
+				break;
+			case SNAKE_HEAD:
+				tile.setFillColor(sf::Color(243,206,10));
+				break;
+			case FOOD:
+				tile.setFillColor(sf::Color(243,127,157));
+				break;
+			default:
+				break;
+		}
+		//draw the tile on the screen
+		window.draw(tile);
+	}
+	//update the window on the screen
+	window.display();
+}
--- src/camera.hpp
+++ src/camera.hpp
@@ -1,8 +1,27 @@
 #ifndef CAMERA_DEF
 #define CAMERA_DEF
+#include "data.hpp"
+#include <SFML/Graphics.hpp>
+#include <utility>
 
 class Camera
 {
+	Data& data;
+	sf::RenderWindow window;
+	sf::RectangleShape tile;
+	std::pair<int, int> const& dimension;
+public:
+	Camera(Data& data):data(data),
+	dimension(data.map.getDimension()),
+		window(sf::VideoMode(data.map.getDimension().first * 50,
+			data.map.getDimension().second * 50), "Snake 1.0"),
+		tile(sf::Vector2f(50.,50.))
+	{
+		tile.setOrigin(7.5,7.5);
+	}
+	
+	//draw the map onto the screen
+	void draw();
 };
 
 #endif//CAMERA_DEF
