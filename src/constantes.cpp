d528c0e147529add7317fa65f14852edc3159781
klonnage <vinc.brdionneau@gmail.com>
2015-08-26 22:40:21 +0200

    constantes du jeu

---
+++ src/constantes.cpp
@@ -0,0 +1,4 @@
+#include "constantes.hpp"
+#include <utility>
+
+const std::pair<int, int> def_dim(17, 17);
--- src/constantes.hpp
+++ src/constantes.hpp
@@ -1,5 +1,6 @@
 #ifndef CONSTANTES_DEF
 #define CONSTANTES_DEF
+#include <utility>
 
 //content cells
 enum CellContent
@@ -7,7 +8,20 @@ enum CellContent
 	EMPTY = 0,
 	WALL = 1,
 	SNAKE_BODY = 2,
-	FOOD = 3
+	FOOD = 3,
+	SNAKE_HEAD = 4
+};
+
+//default dimension
+extern const std::pair<int, int> def_dim;
+
+//orientation
+enum Orientation
+{
+	UP = 0,
+	DOWN = 1,
+	LEFT = 2,
+	RIGHT = 3
 };
 
 #endif//CONSTANTES_DEF
